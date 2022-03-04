84401bd30be5fa2d4be13f12d77564d7fe4e598c
krystianorzechowski1 <93717566+krystianorzechowski1@users.noreply.github.com>
2022-03-04 19:54:20 +0100

    Create script.sql

---
+++ script.sql
@@ -0,0 +1,24 @@
+use spark;
+
+
+CREATE TABLE IF NOT EXISTS assignment_1
+(
+  result float not null
+  
+);
+CREATE TABLE IF NOT EXISTS assignment_2
+(
+  result float not null
+  
+);
+
+CREATE TABLE IF NOT EXISTS assignment_4
+(
+  result float not null
+  
+);
+
+
+CREATE TABLE IF NOT EXISTS assignment_3 (   result float not null, sex varchar(7) not null    );
+CREATE TABLE IF NOT EXISTS assignment_5 (   result float not null, sex varchar(7) not null   );
+CREATE TABLE IF NOT EXISTS assignment_6 (   result float not null, sex varchar(7) not null,   Pclass int );
